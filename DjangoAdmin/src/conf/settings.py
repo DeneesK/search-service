fc85d08cd26106428332f1d91ac1d868bc9bb5d8
D Kalb <kds-ru@list.ru>
2023-04-23 10:53:25 +0200

    build setup fixed

--- DjangoAdmin/src/conf/settings.py
+++ DjangoAdmin/src/conf/settings.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = os.path.dirname(__file__)
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
